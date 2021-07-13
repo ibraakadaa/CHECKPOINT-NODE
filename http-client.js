cf2b283dd32bc9ac50f8ef90d0c53e9b699411cd
ibraakadaa <ibrahimmsahli@gmail.com>
2021-07-13 21:13:26 +0100

    saving 2

--- http-client.js
+++ http-client.js
@@ -8,7 +8,6 @@ if (process.argv[2] !== undefined) {
 
 http.get(urlToGet, function(response) {
 
-	// console.log("Response Status description : " + http.STATUS_CODES[response.statusCode]);
 
 	response.setEncoding("utf8");
 
@@ -16,13 +15,7 @@ http.get(urlToGet, function(response) {
 		console.log(data.toString());
 	});
 
-	// response.on("end", function() {
-	// 	console.log("END");
-	// });
-
-	// response.on("error", function(data) {
-	// 	console.log(data.toString());
-	// });
+	
 
 }).on('error', function(e) {
 	console.log("Got error: " + e.message);
